5ae5025c50df2c1c1f76a169a65b56cf5b6d2e56
vikas <vikaschauhan1995>
2020-07-21 16:21:48 +0530

    Create layout with java activity code

--- app/src/main/java/com/example/googledev/MainActivity.java
+++ app/src/main/java/com/example/googledev/MainActivity.java
@@ -3,13 +3,23 @@ package com.example.googledev;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
         //this is a comment
+        LinearLayout linearL = new LinearLayout(this);
+        linearL.setOrientation(LinearLayout.VERTICAL);
+
+        TextView myText = new TextView(this);
+        myText.setText("Display this text!");
+
+        linearL.addView(myText);
+        setContentView(linearL);
     }
 }
