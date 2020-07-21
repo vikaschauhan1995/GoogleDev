ef9a33d19cb8e489b9662383f1cc7fb628d52d90
vikas <vikaschauhan1995>
2020-07-21 17:55:48 +0530

    click to change the textview (add two buttons to toggle textview)

--- app/src/main/java/com/example/googledev/MainActivity.java
+++ app/src/main/java/com/example/googledev/MainActivity.java
@@ -2,23 +2,28 @@ package com.example.googledev;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView t1;
+    Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //this is a comment
+        t1 = (TextView)findViewById(R.id.t1);
+        b1 = (Button)findViewById(R.id.b1);
+        b1.setOnClickListener(this);
     }
-    public void showToast(View view){
-        String msg = "Hello Toast!";
-        int duration = 2000;
-        @SuppressLint("WrongConstant") Toast toast = Toast.makeText(this, msg, duration);
-        toast.show();
+
+    @Override
+    public void onClick(View view) {
+        t1.setText("You Clicked Button1 ....!!");
+    }
+    public void doSomething(View view){
+        t1.setText("Your Clicked Button2 ....!!");
     }
 }
