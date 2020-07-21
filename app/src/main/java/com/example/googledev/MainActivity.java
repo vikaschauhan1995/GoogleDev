42f90fee291dd23ef7e7ce981963f39861ee80a5
vikas <vikaschauhan1995>
2020-07-21 17:16:40 +0530

    Setting click handler in java

--- app/src/main/java/com/example/googledev/MainActivity.java
+++ app/src/main/java/com/example/googledev/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,11 +15,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //this is a comment
-    }
-    public void showToast(View view){
-        String msg = "Hello Toast!";
-        int duration = 2000;
-        @SuppressLint("WrongConstant") Toast toast = Toast.makeText(this, msg, duration);
-        toast.show();
+        Button button = (Button) findViewById(R.id.button_id);
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                String msg = "Hello Toast!";
+                final int duration = 2000;
+                @SuppressLint("WrongConstant") Toast toast = Toast.makeText(getApplicationContext(), msg, duration);
+                toast.show();
+            }
+        });
     }
 }
