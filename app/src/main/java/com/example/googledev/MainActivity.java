d1502e7c56cc6c14ad39057d3cb9e6f3be91117c
vikas <vikaschauhan1995>
2020-07-21 17:20:39 +0530

    Revert "Setting click handler in java"
    This reverts commit 42f90fee

--- app/src/main/java/com/example/googledev/MainActivity.java
+++ app/src/main/java/com/example/googledev/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,14 +14,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //this is a comment
-        Button button = (Button) findViewById(R.id.button_id);
-        button.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                String msg = "Hello Toast!";
-                final int duration = 2000;
-                @SuppressLint("WrongConstant") Toast toast = Toast.makeText(getApplicationContext(), msg, duration);
-                toast.show();
-            }
-        });
+    }
+    public void showToast(View view){
+        String msg = "Hello Toast!";
+        int duration = 2000;
+        @SuppressLint("WrongConstant") Toast toast = Toast.makeText(this, msg, duration);
+        toast.show();
     }
 }
