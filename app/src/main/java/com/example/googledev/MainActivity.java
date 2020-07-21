2ff1930284ddbcd0ce7e37968896ed9a16da24c4
vikas <vikaschauhan1995>
2020-07-21 16:43:11 +0530

    Handling clicks in XML & java

--- app/src/main/java/com/example/googledev/MainActivity.java
+++ app/src/main/java/com/example/googledev/MainActivity.java
@@ -2,24 +2,23 @@ package com.example.googledev;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
         //this is a comment
-        LinearLayout linearL = new LinearLayout(this);
-        linearL.setOrientation(LinearLayout.VERTICAL);
-
-        TextView myText = new TextView(this);
-        myText.setText("Display this text!");
-
-        linearL.addView(myText);
-        setContentView(linearL);
+    }
+    public void showToast(View view){
+        String msg = "Hello Toast!";
+        int duration = 2000;
+        @SuppressLint("WrongConstant") Toast toast = Toast.makeText(this, msg, duration);
+        toast.show();
     }
 }
