286eb2f44333927b61288285b8637c0d4d9b10f6
vikas <vikaschauhan1995>
2020-07-22 16:39:49 +0530

    Implicit Intent. Click to DIAL mobile no. witch is specified with the help of Intent

--- app/src/main/java/com/example/googledev/MainActivity.java
+++ app/src/main/java/com/example/googledev/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.net.URI;
+
 
 public class MainActivity extends AppCompatActivity{
 
@@ -16,8 +18,14 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
     }
     public void clickToOpenBrowser(View view){
-        Uri uri = Uri.parse("http://www.google.com");
-        Intent it = new Intent(Intent.ACTION_VIEW, uri);
+        Uri uri = Uri.parse("tel:8005551234");
+        Intent it = new Intent(Intent.ACTION_DIAL, uri);
         startActivity(it);
     }
+    // Intent.ACTION_VIEW is for all type
+    // Intent.ACTION_DIAL is specifically for Dial app
+//    it is all upon URI
+//    http: for browser apps
+//    tel: for telephone apps
+//    geo: for map apps
 }
