da27ab7ba0eff69db9507d006dded362da3c2488
vikas <vikaschauhan1995>
2020-07-22 16:02:14 +0530

    Text and scrolling view.

--- app/src/main/java/com/example/googledev/MainActivity.java
+++ app/src/main/java/com/example/googledev/MainActivity.java
@@ -1,31 +1,15 @@
 package com.example.googledev;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-//import Linear.LayoutParams;
 
 public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        LinearLayout linearLayout = new LinearLayout(this);
-        linearLayout.setOrientation(LinearLayout.VERTICAL);
-
-        TextView myTextview =  new TextView(this);
-        myTextview.setWidth(LinearLayout.LayoutParams.MATCH_PARENT);
-        myTextview.setHeight(LinearLayout.LayoutParams.WRAP_CONTENT);
-        myTextview.setMinLines(3);
-        myTextview.setText(R.string.my_story);
-
-        linearLayout.addView(myTextview);
-        setContentView(linearLayout);
-
+        setContentView(R.layout.activity_main);
     }
 }
