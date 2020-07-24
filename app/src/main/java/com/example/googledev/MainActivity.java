d8cbdc46f4393349e6a0c7753899e7ef95b42930
vikas <vikaschauhan1995>
2020-07-24 16:34:25 +0530

    Activity lifecycle in this snepet you will understand how the andriod provide app to the user onCreate, onResume, onPause, onStop, onRestart, onDestroy.

--- app/src/main/java/com/example/googledev/MainActivity.java
+++ app/src/main/java/com/example/googledev/MainActivity.java
@@ -1,25 +1,60 @@
 package com.example.googledev;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity{
-    EditText e1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        e1 = (EditText) findViewById(R.id.edit1);
+        Toast.makeText(this, "onCreate Finished", Toast.LENGTH_SHORT).show();
+        Log.i("MainActivity","onCreate Appps");
     }
 
-    public void doSomething(View view) {
-        Intent i1 = new Intent(this, Second.class);
-        i1.putExtra("user",e1.getText().toString());
-        startActivity(i1);
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "onStart Finished", Toast.LENGTH_SHORT).show();
+        Log.i("MainActivity","onStart Appps");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "onResume Finished", Toast.LENGTH_SHORT).show();
+        Log.i("MainActivity","onResume Appps");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "onPause Finished", Toast.LENGTH_SHORT).show();
+        Log.i("MainActivity","onPause Appps");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "onStop Finished", Toast.LENGTH_SHORT).show();
+        Log.i("MainActivity","onStop Appps");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "onRestart Finished", Toast.LENGTH_SHORT).show();
+        Log.i("MainActivity","onRestart Appps");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "onDestroy Finished", Toast.LENGTH_SHORT).show();
+        Log.i("MainActivity","onDestroy Appps");
     }
 }
