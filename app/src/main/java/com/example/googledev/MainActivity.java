0e8b44bd2c118e4ce25c122acd1bbb59179901e9
vikas <vikaschauhan1995>
2020-07-30 00:01:17 +0530

    Recycler view print a list with a kind of a loop.

--- app/src/main/java/com/example/googledev/MainActivity.java
+++ app/src/main/java/com/example/googledev/MainActivity.java
@@ -2,80 +2,37 @@ package com.example.googledev;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.viewpager.widget.ViewPager;
-
-import com.google.android.material.tabs.TabLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity{
 
-    TabLayout myTab;
-    ViewPager myPage;
+    RecyclerView r1;
+    String s1[], s2[];
+    int imageResourse[] = {
+            R.drawable.cat_image,
+            R.drawable.cat_image,
+            R.drawable.cat_image,
+            R.drawable.cat_image,
+            R.drawable.cat_image,
+            R.drawable.cat_image,
+            R.drawable.cat_image,
+            R.drawable.cat_image};
+    MyOwnAdapter ad;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        myTab = (TabLayout) findViewById(R.id.myTab);
-        myPage = (ViewPager) findViewById(R.id.myPager);
+        r1 = (RecyclerView)findViewById(R.id.myRecycler);
 
-        myPage.setAdapter(new MyOwnPagerAdapter(getSupportFragmentManager()));
-        myTab.setupWithViewPager(myPage);
+        s1 = getResources().getStringArray(R.array.pet_name);
+        s2 = getResources().getStringArray(R.array.disc);
 
-        myTab.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                myPage.setCurrentItem(tab.getPosition());
-            }
+        ad = new MyOwnAdapter(this,s1,s2,imageResourse);
 
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
-
-    }
-    class MyOwnPagerAdapter extends FragmentPagerAdapter {
-
-        String data[] = {"Java", "Android", "iOS"};
-
-        public MyOwnPagerAdapter(@NonNull FragmentManager fm) {
-            super(fm);
-        }
-
-        @NonNull
-        @Override
-        public Fragment getItem(int position) {
-            if (position == 0) {
-                return new Java();
-            }
-            if(position == 1){
-                return new Android();
-            }
-            if(position == 2){
-                return new IOS();
-            }
-            return null;
-        }
-
-        @Override
-        public int getCount() {
-            return data.length;
-        }
-
-        @Nullable
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return data[position];
-        }
+        r1.setAdapter(ad);
+        r1.setLayoutManager(new LinearLayoutManager(this));
     }
 }
