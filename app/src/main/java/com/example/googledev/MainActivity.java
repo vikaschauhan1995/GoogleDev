a22e292e5f9ab48265f7f38b9f01e5f62c39fbf2
vikas <vikaschauhan1995>
2020-08-02 18:19:09 +0530

    Material Design

--- app/src/main/java/com/example/googledev/MainActivity.java
+++ app/src/main/java/com/example/googledev/MainActivity.java
@@ -1,9 +1,10 @@
 package com.example.googledev;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,7 +15,16 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
-    public void doImage(View view) {
-        Toast.makeText(this, "Image Button clicked", Toast.LENGTH_SHORT).show();
+    public void doSomething(View view) {
+        AlertDialog.Builder myAlert = new AlertDialog.Builder(this);
+        myAlert.setTitle("ADD");
+        myAlert.setMessage("Data is added");
+        myAlert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        myAlert.show();
     }
 }
