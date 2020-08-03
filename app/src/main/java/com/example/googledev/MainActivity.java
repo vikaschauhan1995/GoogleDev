60e5b39382365b5bb28aab499fb518029b2142a4
vikas <vikaschauhan1995>
2020-08-03 16:54:21 +0530

    Material Design

--- app/src/main/java/com/example/googledev/MainActivity.java
+++ app/src/main/java/com/example/googledev/MainActivity.java
@@ -1,30 +1,13 @@
 package com.example.googledev;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void doSomething(View view) {
-        AlertDialog.Builder myAlert = new AlertDialog.Builder(this);
-        myAlert.setTitle("ADD");
-        myAlert.setMessage("Data is added");
-        myAlert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                dialogInterface.dismiss();
-            }
-        });
-        myAlert.show();
     }
 }
